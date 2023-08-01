46a098ad438585a3458a84c55469b1d16fae0c39
FaffyBanana <52312066+FaffyBanana@users.noreply.github.com>
2023-08-01 17:11:43 +0100

    Initial commit

---
+++ Source/ScifiFPS/ScifiFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ScifiFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AScifiFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AScifiFPSGameMode();
+};
+
+
+
