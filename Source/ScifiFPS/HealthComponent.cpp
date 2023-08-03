5774b7c3e69a338d9208f504b340b68e31f766b6
JaberHQ <jaber4hmed@gmail.com>
2023-08-03 20:00:04 +0100

    Player Health Bar (BP)
    - Created a BP widget health bar
    - Created a Health Component to deal with Player and Enemy health

---
+++ Source/ScifiFPS/HealthComponent.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	m_maxHealth = 100;
+	m_health = m_maxHealth;
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+int UHealthComponent::GetHealth()
+{
+	return m_health;
+}
+
+int UHealthComponent::GetMaxHealth()
+{
+	return m_maxHealth;
+}
+
